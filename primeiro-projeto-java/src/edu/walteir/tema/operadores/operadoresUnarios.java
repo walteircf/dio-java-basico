d91e031abb2d7f5446ebcc4c8060f062f1a96041
Walteir Coelho <wcfreitasf@gmail.com>
2025-06-24 22:05:24 -0300

    feat: Implementa a classe operadoresUnarios explicando a forma de declaração e uso dos operadores

--- primeiro-projeto-java/src/edu/walteir/tema/operadores/operadoresUnarios.java
+++ primeiro-projeto-java/src/edu/walteir/tema/operadores/operadoresUnarios.java
@@ -1,5 +1,28 @@
 package edu.walteir.tema.operadores;
 
 public class operadoresUnarios {
+    // São utilizados juntamente com um outro operador aritmetico para incrementar, decrementar, inverter valores e booleans
 
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println(- numero); //operador unario para tornar o numero negativo, mas altera apenas o resultado, nao a variavel
+        System.out.println(numero);
+
+        //Para alterar a variavel para negativo é necessário:
+        numero = - numero;
+        System.out.println(numero);
+        
+        //Para torná-lo positivo novamente é necessário:
+        numero = numero * -1;
+        System.out.println(numero);
+
+        numero++; //operador unario que incrementa a variavel
+        System.out.println(numero); 
+
+        numero = 10;
+        System.out.println(numero);
+        numero--; //valor unario que decrementa a variável
+        System.out.println(numero);
+
+    }
 }
