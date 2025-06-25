3e926031f92477a23c641cfc4de43330b807557e
Walteir Coelho <wcfreitasf@gmail.com>
2025-06-24 21:02:51 -0300

    feat: Adiciona a classe operadorAtribuicao e implementa suas informações e formas de declaração

---
+++ primeiro-projeto-java/src/edu/walteir/tema/operadores/operadorAtribuicao.java
@@ -0,0 +1,17 @@
+package edu.walteir.tema.operadores;
+
+public class operadorAtribuicao {
+    public static void main(String[] args) {
+        //Representado pelo simbolo de igualdade '='
+
+        //Define o valor ou sobrescreve o valor de uma variável
+
+        String nome = "Walteir"; //atribui o valor entre "aspas duplas"
+        int idade = 28; //atribui o valor literal numerico
+        double peso = 101.50; //atribui valor literal usando o ponto flutuante para separar o decimal
+        char sexo = 'M'; //atribui o valor entre 'aspas simples'
+        boolean doadorOrgao = false; //atribui usando valores literais "false" ou "true"
+        //Date dataNascimento = new Date(); variáveis do tipo "object" inclui-se o "new" e o valor entre os (parenteses)
+
+    }
+}
