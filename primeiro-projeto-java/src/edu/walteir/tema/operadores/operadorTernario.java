32e8f0a1a8a631d14ce686e2081bac8d50b4710a
Walteir Coelho <wcfreitasf@gmail.com>
2025-06-24 22:19:26 -0300

    feat: Implementa a classe de operadorTernario explicando sua declaração e uso

--- primeiro-projeto-java/src/edu/walteir/tema/operadores/operadorTernario.java
+++ primeiro-projeto-java/src/edu/walteir/tema/operadores/operadorTernario.java
@@ -1,5 +1,24 @@
 package edu.walteir.tema.operadores;
 
 public class operadorTernario {
+// É uma forma resumida de definir uma condição de escolha entre valores, como uma estrutura "if/else", mas escrita em apenas uma linha
+    public static void main(String[] args) {
+        //<expressão condicional> ? <condição se for verdafeiro> : <condição se for falsa>:
+        int a, b, c;
 
+        a = 8;
+        b = 50;
+        c = 7;
+
+        String resultado = a < b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+        //No caso de mais de 2 valores
+        //<expressão condicional 1> ? <condição se verdadeiro : <expressão condicional 2> ? < expressão se verdadeiro> : <condição se ambas falsas>
+
+        String maior = a > b && a > c ? "a é o maior" : b > c && b > a ? "b é o maior" : "c é o maior";
+
+        System.out.println(maior);
+    }
 }
--- primeiro-projeto-java/src/edu/walteir/tema/operadores/operadoresUnarios.java
+++ primeiro-projeto-java/src/edu/walteir/tema/operadores/operadoresUnarios.java
@@ -5,7 +5,7 @@ public class operadoresUnarios {
 
     public static void main(String[] args) {
         int numero = 5;
-        System.out.println(- numero); //operador unario para tornar o numero negativo, mas altera apenas o resultado, nao a variavel
+        System.out.println(- numero); //operador unario para tornar o numero negativo, mas altera apenas em memoria, nao a variavel
         System.out.println(numero);
 
         //Para alterar a variavel para negativo é necessário:
@@ -16,13 +16,20 @@ public class operadoresUnarios {
         numero = numero * -1;
         System.out.println(numero);
 
-        numero++; //operador unario que incrementa a variavel
+        ++numero; //operador unario que incrementa a variavel
+        numero++; //apresenta o numero e o incrementa depois
         System.out.println(numero); 
 
         numero = 10;
         System.out.println(numero);
         numero--; //valor unario que decrementa a variável
         System.out.println(numero);
+
+        //Para incrementar valores de 2 ou mais usamos:
+        numero += 3; //incrementa 3
+        numero *= 2; 
+        numero -= 2;
+        numero /= 2;
 
     }
 }
