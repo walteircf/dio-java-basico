da1a28a72a1086136ca67d50e7bb44117e8186db
Walteir Coelho <wcfreitasf@gmail.com>
2025-06-24 22:42:32 -0300

    feat: Implementa a classe operadoresRelacionais com a forma de declaração e utilização dos operadores

--- primeiro-projeto-java/src/edu/walteir/tema/operadores/operadoresRelacionais.java
+++ primeiro-projeto-java/src/edu/walteir/tema/operadores/operadoresRelacionais.java
@@ -1,5 +1,43 @@
 package edu.walteir.tema.operadores;
 
 public class operadoresRelacionais {
+// Operadores relacionais avaliam a relação entre duas variáveis ou expressões.
+    public static void main(String[] args) {
+        int a, b, c;
 
+        a = 7;
+        b = 7;
+        c = 5;
+
+        if (a > b) {
+            System.out.println("a é maior");
+        }
+        if (a < b) {
+            System.out.println("a é menor");
+        }
+        if (a == b) {
+            System.out.println("a é igual a b");
+        } else { 
+            System.out.println("a é diferente de b");
+        }
+            
+        
+        if (a >= c) {
+            System.out.println("a é maior ou igual a c");
+        }
+        if (a <= c) {
+            System.out.println("a é menor ou igual a c");
+        }
+        
+        String nome1 = "Heloisa";
+        String nome2 = "Heloisa";
+
+        System.out.println(nome1 == nome2 ? "Conteúdo igual" : "Conteúdo diferente"); //é possível comparar conteúdo
+    
+        String nomeUm = "Walteir";
+        String nomeDois = new String("Walteir");
+
+        System.out.println(nomeUm.equals(nomeDois) ? "Conteúdo igual" : "Conteúdo diferente"); //para comparar objetos ou textos, é necessário utilizar a função ".equals()" como no exemplo, passando uma variável.equals(outraVariavel)
+
+    }
 }
