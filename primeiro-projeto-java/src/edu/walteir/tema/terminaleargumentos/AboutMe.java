9b211d5e36fffddeb091dd35b68f540333432402
Walteir Coelho <wcfreitasf@gmail.com>
2025-07-02 10:46:06 -0300

    update: atualiza classe terminaleargumentos

--- primeiro-projeto-java/src/edu/walteir/tema/terminaleargumentos/AboutMe.java
+++ primeiro-projeto-java/src/edu/walteir/tema/terminaleargumentos/AboutMe.java
@@ -1,14 +1,26 @@
 package edu.walteir.tema.terminaleargumentos;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class AboutMe {
     public static void main(String[] args) {
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + " cm ");
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Olá, me chamo " + nome
+                + " " + sobrenome
+                + ", tenho " + idade + " anos "
+                + "e minha altura é " + altura + "cm ");
     }
 }
